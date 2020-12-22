8d2196358f6efcfff28107964b3c729409a4cbc7
BartoszKowalczyk98 <kowalczykbar98@gmail.com>
2020-12-22 14:30:41 +0100

    temporarly removed getjobs from list of endpoints because i intend to remake it soon, index is now showing and can lead to another endpoint with some data in request, also added support for localization

--- src/main/java/com/Bartosz/vCard/VCard.java
+++ src/main/java/com/Bartosz/vCard/VCard.java
@@ -7,7 +7,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,23 +17,31 @@ import java.util.List;
 @RestController
 public class VCard {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView index() {return new ModelAndView("index", "SearchItem", new SearchItem()); }
+	public ModelAndView index() {
+		return new ModelAndView("index", "index", new SearchItem());
+	}
 
 	//https://panoramafirm.pl/szukaj?k=hydraulik&l=
-	@RequestMapping(value = "/getjobs", method = RequestMethod.GET)
-	public List<DataFromJson> getJobs(@RequestParam("k") String searchitem) {
+	public List<DataFromJson> getJobs(String searchitem, String localization) {
 		//jsoup beginners guide taken from https://jsoup.org/cookbook/input/load-document-from-url
 		List<DataFromJson> listOfOffers = new ArrayList<>();
 
 		try {
 			StringBuilder stringBuilder = new StringBuilder();
+			if (searchitem == null || searchitem.isBlank())
+				return listOfOffers;
+
 			stringBuilder.append("https://panoramafirm.pl/szukaj?k=").append(searchitem).append("&l=");
+
+			if (localization != null && !localization.isEmpty())
+				stringBuilder.append(localization);
+
 			Document doc = Jsoup.connect(stringBuilder.toString()).get();
 			Elements jsons = doc.select("script");
 			Gson gson = new Gson();
 
 			for (Element element : jsons) {
-				if (element.attr("type").equals("application/ld+json") && element.data().contains("LocalBusiness")) {
+				if (element.attr("type").equals("application/ld+json") && element.data().contains("Address")) {
 					DataFromJson singleCompanyInfo = gson.fromJson(element.data(), DataFromJson.class);
 					listOfOffers.add(singleCompanyInfo);
 				}
